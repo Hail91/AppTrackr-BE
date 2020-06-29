2d98962fbd9a8adfea97a05d67864fcabe56455a
Aaron <aarongillies88@gmail.com>
2020-06-29 16:12:07 -0400

    finished setting up dbconfig and knexfile

---
+++ data/dbConfig.js
@@ -0,0 +1,9 @@
+const knex = require("knex");
+
+const knexConfig = require("../knexfile");
+
+const dbEnv = process.env.DB_ENV || "development";
+
+const db = knex(knexConfig[dbEnv]);
+
+module.exports = db;
--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,52 @@
-// Update with your config settings.
+require("dotenv").config();
 
 module.exports = {
-
+  // Development DB settings
   development: {
-    client: 'sqlite3',
+    client: "postgresql",
     connection: {
-      filename: './dev.sqlite3'
-    }
+      connection: process.env.DATABASE_URL,
+    },
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
   },
-
+  // Staging DB settings (Probably won't be used)
   staging: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password",
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: "knex_migrations",
+    },
   },
-
+  // Production DB settings
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: "postgresql",
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
 };
